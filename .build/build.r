db35360b523d130226eef40e31deba270c7ddd0a
cfmack <cfmack@gmail.com>
2020-03-04 22:43:54 -0600

    start building on travis

--- .build/build.r
+++ .build/build.r
@@ -1,12 +1,10 @@
-install.packages("installr", contriburl="https://cloud.r-project.org/bin/windows/contrib/3.5/")
-install.packages("devtools", contriburl="https://cloud.r-project.org/bin/windows/contrib/3.5/")
-install.packages("roxygen2", contriburl="https://cloud.r-project.org/bin/windows/contrib/3.5/")
+install.packages("devtools")
+install.packages("roxygen2")
 
-library("installr")
-install.Rtools()
 
 library("devtools")
 library("roxygen2")
 
 setwd("./pactr")
 devtools::document(roclets=c('rd', 'collate', 'namespace'))
+devtools::build(".") 
